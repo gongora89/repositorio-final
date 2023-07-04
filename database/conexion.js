f0b81d596d0447ceb5188152b05211830647b72e
Santiago Góngora <90485321+gongora89@users.noreply.github.com>
2023-07-03 20:25:19 -0500

    proyecto final del curso

--- database/conexion.js
+++ database/conexion.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'veterinaria'
+  password: 'root',
+  database: 'hospital'
 });
 
 // Conexión a la base de datos
--- routes/citas.js
+++ routes/citas.js
@@ -5,12 +5,12 @@ const {connection} = require('../database/conexion.js')
 /* Obtener citas medicas*/
 
 router.get('/', function(req, res, next) {
-  connection.query('SELECT c.id, c.fecha, ma.nombre, med.nombres, med.consultorio FROM cita_medica c, mascotas ma, medicos med WHERE ma.cedula_duenio=id_mascota AND med.cedula=id_medico;', function(error, results) {
+  connection.query('SELECT c.id, c.fecha, pa.nombre, med.nombres, med.consultorio FROM cita_medica c, pacientes pa, medicos med WHERE pa.cedula=cedula_paciente AND med.cedula=cedula_medico;', function(error, results) {
     if (error) {
       console.log("Error en la consulta", error)
       res.status(500).send("Error en la consulta");
     }else{
-    res.render('citas', { citas: results });
+    res.render('citas', {citas: results });
     }
   });
 });
@@ -22,7 +22,7 @@ router.get('/agregar-cita', (req, res) =>{
 })
 
 router.post('/agregar', (req, res) =>{
-  const cedulaDuenio = req.body.cedula;
+  const cedulapaciente = req.body.cedula;
   const fecha = req.body.fecha;
   const especialidad = req.body.especialidad;
 
@@ -31,8 +31,9 @@ router.post('/agregar', (req, res) =>{
       console.log("Error en la consulta", error)
       res.status(500).send("Error en la consulta");
     }
-      const cedulaMedico = results[0].cedula;
-      connection.query(`INSERT INTO cita_medica (id_mascota, id_medico, fecha) VALUES (${cedulaDuenio}, ${cedulaMedico}, '${fecha}')`, (error, result) => {
+      const cedulaMedico = results[0].cedula; 
+      console.log(cedulaMedico)
+      connection.query(`INSERT INTO cita_medica (cedula_paciente, cedula_medico, fecha) VALUES (${cedulapaciente}, ${cedulaMedico}, '${fecha}')`, (error, result) => {
         if (error) {
           console.log("Ocurrio un error en la ejecución", error)
           res.status(500).send("Error en la consulta");
